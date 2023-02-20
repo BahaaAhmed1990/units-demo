08f787cbb8757f2cbb993bab67b3d8a2d0e51d15
Bahaa Ahmed <bahaaahmed19902012@gmail.com>
2023-02-21 00:27:39 +0200

    connect to google sheet

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -44,7 +44,38 @@ const imgItem = document.createElement("img");
       });
   });
 });
+const sheetId = "1dEy8bMqOo1cFngZpmf2hWQZpZedDITPXoGnAoTbxb2g";
+const sheetTitle = "state";
+const sheetRange = "A1:B9";
+let fullUrl =
+  "https://docs.google.com/spreadsheets/d/" +
+  sheetId +
+  "/gviz/tq?sheet=" +
+  sheetTitle +
+  "&range=" +
+  sheetRange;
+
+fetch(fullUrl)
+  .then((res) => res.text())
+  .then((rep) => {
+    let data = JSON.parse(rep.substr(47).slice(0, -2));
+    let rows = data.table.rows;
+    // console.log(row.c[1].v)});
+    console.log(rows);
+
+    rows.map((row) => {
+      if (row.c[1].v === "available") {
+        let unitId = `#label-${row.c[0].v}`;
+        console.log(unitId);
+        document.querySelector(unitId).style.backgroundColor = "#00800094";
+      }
+      if (row.c[1].v === "sold") {
+        let unitId = `#label-${row.c[0].v}`;
+        console.log(unitId);
+        document.querySelector(unitId).style.backgroundColor = "#d1595994";
+      }
+    });
+  });
 // function loadImg(e) {
 // e.preventDefault();
 // content.innerHTML = "<img src='./assets/images/blue-1.jpg' />";
-// }
