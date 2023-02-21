af910d3e10b4d30492ab15b38f2c125adfa2be88
Bahaa Ahmed <bahaaahmed19902012@gmail.com>
2023-02-22 01:40:21 +0200

    modify unit types

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -2,8 +2,12 @@ const select = document.querySelectorAll(".form-select");
 const gallery = document.querySelector("#gallery");
 const errMsg = document.createElement("h3");
 const imgItem = document.createElement("img");
+const tableRow = document.createElement("tr");
+const tableBody = document.querySelector("#tabel-body");
+const labels = document.querySelectorAll(".my-label");
 // console.log(select);
 
+// choose unit
 [...select].map((elem) => {
   elem.addEventListener("change", function (e) {
     axios
@@ -44,9 +48,11 @@ const imgItem = document.createElement("img");
       });
   });
 });
+
+// fetch google sheet
 const sheetId = "1dEy8bMqOo1cFngZpmf2hWQZpZedDITPXoGnAoTbxb2g";
 const sheetTitle = "state";
-const sheetRange = "A1:B9";
+const sheetRange = "A2:F10";
 let fullUrl =
   "https://docs.google.com/spreadsheets/d/" +
   sheetId +
@@ -57,25 +63,70 @@ let fullUrl =
 
 fetch(fullUrl)
   .then((res) => res.text())
+  // modify spans
   .then((rep) => {
     let data = JSON.parse(rep.substr(47).slice(0, -2));
     let rows = data.table.rows;
     // console.log(row.c[1].v)});
-    console.log(rows);
+    // console.log(rows);
 
     rows.map((row) => {
       if (row.c[1].v === "available") {
         let unitId = `#label-${row.c[0].v}`;
-        console.log(unitId);
+
         document.querySelector(unitId).style.backgroundColor = "#00800094";
       }
       if (row.c[1].v === "sold") {
         let unitId = `#label-${row.c[0].v}`;
-        console.log(unitId);
         document.querySelector(unitId).style.backgroundColor = "#d1595994";
       }
     });
+    return rows;
+  })
+  // create table
+  .then((rows) => {
+    // const cells = rows.c;
+    // console.log(cells);
+    rows.map((row) => {
+      // console.log(row.c);
+      tableBody.innerHTML += `
+      <tr class='units-rows' id='${row.c[0].v}'><td>${row.c[0].v}</td>
+      <td>${row.c[2].v}</td>
+      <td>${row.c[4].v}</td>
+      <td>${row.c[3].v}</td>
+      <td>${row.c[5].v}</td>
+      </tr>`;
+      const unitsRows = document.querySelectorAll(".units-rows");
+      [...unitsRows].map((row) => {
+        row.addEventListener("mouseenter", function () {
+          console.log(this.id);
+          document.querySelector(`#label-${this.id}`).style.opacity = ".5";
+        });
+        row.addEventListener("mouseleave", function () {
+          console.log(this.id);
+          document.querySelector(`#label-${this.id}`).style.opacity = "1";
+        });
+      });
+    });
+    return rows;
+  })
+  // modify modal
+  .then((rows) => {
+    console.log(rows);
+    [...labels].map((label) => {
+      label.addEventListener("click", function () {
+        let rowNum = Number(this.innerText);
+        console.log(rows[rowNum - 1].c[3].v);
+        document.querySelector("#staticBackdropLabel").textContent =
+          rows[rowNum - 1].c[3].v;
+
+        document.querySelector("#rooms").textContent = rows[rowNum - 1].c[4].v;
+
+        document.querySelector("#unit-code").textContent =
+          rows[rowNum - 1].c[2].v;
+
+        document.querySelector("#sellable-area").textContent =
+          rows[rowNum - 1].c[5].v;
+      });
+    });
   });
-// function loadImg(e) {
-// e.preventDefault();
-// content.innerHTML = "<img src='./assets/images/blue-1.jpg' />";
