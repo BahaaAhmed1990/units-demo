51af1a42581f0e10f99ee75c9daabcd94c79c2f5
Bahaa Ahmed <bahaaahmed19902012@gmail.com>
2023-03-01 00:25:09 +0200

    add modal for virtual page

--- frontend/assets/js/virtual-tour.js
+++ frontend/assets/js/virtual-tour.js
@@ -1,6 +1,12 @@
 const tableRow = document.createElement("tr");
 const tableBody = document.querySelector("#tabel-body");
 const labels = document.querySelectorAll(".my-label");
+const unitInfo = document.querySelector("#unit-info");
+const btn = document.querySelector("#unit-info-close");
+const builidImg = document.querySelector("#builiding-img");
+const apartImg = document.querySelector("#apartement-img");
+const defaultImg = document.querySelector("#default-img");
+
 // fetch google sheet
 const sheetId = "1dEy8bMqOo1cFngZpmf2hWQZpZedDITPXoGnAoTbxb2g";
 const sheetTitle = "state";
@@ -24,13 +30,9 @@ fetch(fullUrl)
 
     rows.map((row) => {
       if (row.c[1].v === "available") {
-        let unitId = `#label-${row.c[0].v}`;
+        let unitId = `#${row.c[2].v}`;
         document.querySelector(unitId).style.visibility = "visible";
       }
-      // if (row.c[1].v === "sold") {
-      // let unitId = `#label-${row.c[0].v}`;
-      // document.querySelector(unitId).style.backgroundColor = "#d1595994";
-      // }
     });
     return rows;
   })
@@ -42,54 +44,70 @@ fetch(fullUrl)
       // console.log(row.c);
       if (row.c[1].v === "available") {
         tableBody.innerHTML += `
-      <tr class='units-rows' id='${row.c[0].v}'>
-      <td>${row.c[2].v}</td>
-      <td>${row.c[4].v}</td>
-      <td>${row.c[3].v}</td>
-      <td>${row.c[5].v}</td>
-      </tr>`;
+    <tr class='units-rows' id='r-${row.c[2].v}'>
+    <td>${row.c[2].v}</td>
+    <td>${row.c[4].v}</td>
+    <td>${row.c[3].v}</td>
+    <td>${row.c[5].v}</td>
+    </tr>`;
       }
 
       const unitsRows = document.querySelectorAll(".units-rows");
       [...unitsRows].map((row) => {
         row.addEventListener("mouseenter", function () {
-          // console.log(this.id);
-          document.querySelector(`#label-${this.id}`).style.opacity = ".7";
+          let rowId = String(this.id.slice(2));
+          // console.log(rowId);
+          document.querySelector(`#${rowId}`).style.opacity = ".7";
         });
         row.addEventListener("mouseleave", function () {
-          // console.log(this.id);
-          document.querySelector(`#label-${this.id}`).style.opacity = "1";
+          let rowId = this.id.slice(2);
+          document.querySelector(`#${rowId}`).style.opacity = "1";
+        });
+        row.addEventListener("click", function () {
+          unitInfo.style.display = "block";
+          document.body.style.overflow = "hidden";
+          let rowId = this.id.slice(2);
+          console.log(rowId);
+
+          defaultImg.setAttribute(
+            "src",
+            `./assets/images/${rowId}/floor-plan.webp`
+          );
+
+          builidImg.setAttribute(
+            "src",
+            `./assets/images/${rowId}/builiding.png`
+          );
+          apartImg.setAttribute(
+            "src",
+            `./assets/images/${rowId}/floor-plan.webp`
+          );
+          document.getElementById("defaultOpen").click();
         });
       });
     });
     return rows;
   });
-// // modify modal
-// .then((rows) => {
-//   console.log(rows);
-//   [...labels].map((label) => {
-//     label.addEventListener("click", function () {
-//       let rowNum = Number(this.innerText);
-//       console.log(rows[rowNum - 1].c[3].v);
-//       document.querySelector("#staticBackdropLabel").textContent =
-//         rows[rowNum - 1].c[3].v;
 
-//       document.querySelector("#rooms").textContent = rows[rowNum - 1].c[4].v;
+// close modal
+btn.addEventListener("click", function () {
+  unitInfo.style.display = "none";
+  document.body.style.overflow = "auto";
+});
 
-//       document.querySelector("#unit-code").textContent =
-//         rows[rowNum - 1].c[2].v;
+// open tab function for model
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-//       document.querySelector("#sellable-area").textContent =
-//         rows[rowNum - 1].c[5].v;
-//     });
-//   });
-// });
-
-// // pin the table
-// ScrollTrigger.create({
-//   trigger: "#table",
-//   pin: true,
-//   onEnter: () => {
-//     console.log(" enter");
-//   },
-// });
+// Get the element with id="defaultOpen" and click on it
